1c9650fd63cbf4ded137ba23ced2165e29a91a78
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-20 05:25:53 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -5,7 +5,8 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-
+    @school_class = School_class.new
+    redirect_to school_class_path(@school_class)
   end
 
   def show
