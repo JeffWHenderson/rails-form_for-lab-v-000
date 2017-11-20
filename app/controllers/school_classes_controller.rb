a7e0762eeb48880c37225e57c3047b86fa70b344
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-20 05:43:46 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -18,7 +18,9 @@ class SchoolClassesController < ApplicationController
   end
 
   def update
-
+    @school_class.find(params[:id])
+    @school_class.update(school_class_params(:title, :room_number))
+    redirect_to school_class_path(@school_class)
   end
 
   private
